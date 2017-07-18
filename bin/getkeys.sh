7de492db922860f6d7fa3dd64cfc91f0a5359532
Nikolay Simakov <nikolays@buffalo.edu>
2017-07-18 17:00:52 -0400

    no need for key gen script now

--- bin/getkeys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-MYPROXY_SERVER=myproxy.teragrid.org
-MYPROXY_SERVER_PORT=7514
-export MYPROXY_SERVER MYPROXY_SERVER_PORT
-myproxy-logon -S -T -t 24000 -l xdtas << END
-@pp+k3rnel_Runner
-END
-
