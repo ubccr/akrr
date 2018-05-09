8ccb70126a83f6525f56cf7c45387bbff7ffb108
Nikolay Simakov <nikolays@buffalo.edu>
2018-05-09 12:48:54 -0400

    added default value for REPO_FULL_NAME

--- tests/regtest1/run_test_on_shippable.sh
+++ tests/regtest1/run_test_on_shippable.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
-
+export REPO_FULL_NAME=${REPO_FULL_NAME:-ubccr/akrr}
 
 set -e
+
 # Start all daemons
 cmd_start self_contained_slurm_wlm
 squeue
 sinfo
 ps -Af
+
 # Build RPM
-cd /root/src/github.com/$REPO_FULL_NAME
+cd /root/src/github.com/${REPO_FULL_NAME}
 ./setup.py bdist_rpm
+
 # Install RPM
 rpm -Uvh dist/akrr-*.noarch.rpm
 
@@ -18,6 +21,7 @@ mkdir -p shippable/testresults
 mkdir -p shippable/codecoverage
 pytest --junitxml=shippable/testresults/testresults.xml
 pytest --cov=akrr --cov-report=xml:shippable/codecoverage/coverage.xml
-# Run regtest
-export PATH=/root/src/github.com/$REPO_FULL_NAME/tests/bin:$PATH
-/root/src/github.com/$REPO_FULL_NAME/tests/regtest1/run_test.sh
+
+# Run this regression test
+export PATH=/root/src/github.com/${REPO_FULL_NAME}/tests/bin:$PATH
+/root/src/github.com/${REPO_FULL_NAME}/tests/regtest1/run_test.sh
