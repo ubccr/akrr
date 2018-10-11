db478333666f60179ebf4598f26a023ff8f6e95f
Nikolay Simakov <nikolays@buffalo.edu>
2018-10-11 18:52:22 -0400

    Added OpenStack CLI wrapper with simple unit test

---
+++ tests/unit_tests/util/openstack_test.py
@@ -0,0 +1,26 @@
+"""
+Unit tests for open stack. Currently works on computers with openstack_env_set.sh script which set all environment
+variables.
+
+Run at root of akrr git repo:
+PYTHONPATH=`pwd` pytest -m "openstack"  ./tests/unit_tests
+"""
+import pytest
+
+
+@pytest.mark.openstack
+def test_openstack():
+    return
+
+
+@pytest.mark.openstack
+def test_openstack_expired_token():
+    from akrr.util import log
+    from akrr.util.openstack import OpenStack
+    log.set_verbose()
+    ostack = OpenStack()
+    out = ostack.run_open_stack_cmd("image list")
+    assert out.count("ID") > 0 and out.count("Name") > 0 and out.count("Status") > 0
+    ostack.token_revoke()
+    out = ostack.run_open_stack_cmd("image list")
+    assert out.count("ID") > 0 and out.count("Name") > 0 and out.count("Status") > 0
