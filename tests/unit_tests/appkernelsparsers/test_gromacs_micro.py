ff334e052e9d27c3ab70e154efbf90df148f4f5f
Nikolay Simakov <nikolays@buffalo.edu>
2019-01-03 14:44:22 -0500

    added asserts to gromacs_micro parser test

--- tests/unit_tests/appkernelsparsers/test_gromacs_micro.py
+++ tests/unit_tests/appkernelsparsers/test_gromacs_micro.py
@@ -1,10 +1,39 @@
 def test_parser(datadir):
     from akrr.appkernelsparsers.app_md_gromacs_micro import process_appker_output
+    import xml.etree.ElementTree as ElementTree
+    from akrr.util import floats_are_close
+    from .xml_comparison import parstat_val, parstat_val_f, parstat_val_i
+
     results = process_appker_output(
-        appstdout=str(datadir.join('appstdout')),
-        stdout=str(datadir.join('stdout')),
-        stderr=str(datadir.join('stderr')),
-        geninfo=str(datadir.join('gen.info')),
+        appstdout=str(datadir / 'appstdout'),
+        stdout=str(datadir / 'stdout'),
+        stderr=str(datadir / 'stderr'),
+        geninfo=str(datadir / 'gen.info'),
         resource_appker_vars={'resource': 'HPC-Cluster'}
     )
-    print(results)
+    # check resulting xml
+    xml_out = ElementTree.fromstring(results.replace("'", ""))
+    params = xml_out.find(".//parameters")
+    stats = xml_out.find(".//statistics")
+
+    assert xml_out.find('./exitStatus/completed').text == "true"
+
+    # Compare parameters to reference
+    assert len(parstat_val(params, "App:ExeBinSignature")) > 5
+    assert len(parstat_val(params, "RunEnv:Nodes")) > 5
+    assert parstat_val(params, "resource") == "HPC-Cluster"
+    assert floats_are_close(parstat_val_f(params, "App:Version"), 2018.4)
+
+    # Compare stats to reference
+    assert parstat_val_i(stats, "App kernel executable exists") == 1
+    assert parstat_val_i(stats, "App kernel input exists") == 1
+    assert parstat_val_i(stats, "Task working directory accessible") == 1
+    assert parstat_val_i(stats, "Task working directory exists") == 1
+    assert parstat_val_i(stats, "local scratch directory accessible") == 1
+    assert parstat_val_i(stats, "local scratch directory exists") == 1
+    assert parstat_val_i(stats, "Network scratch directory accessible") == 1
+    assert parstat_val_i(stats, "Network scratch directory exists") == 1
+
+    assert floats_are_close(parstat_val_f(stats, "Core Clock Time"), 16.978)
+    assert floats_are_close(parstat_val_f(stats, "Simulation Speed"), 12.336)
+    assert floats_are_close(parstat_val_f(stats, "Wall Clock Time"), 1.415)
