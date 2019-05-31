4ffa729f83010dc05e436841ce72493188febf1a
hoffmaps <hoffmaps@lemoyne.edu>
2019-05-31 11:33:05 -0400

    un-commented out section at end

--- docker/hpcc/scripts/setup_hpcc_input.sh
+++ docker/hpcc/scripts/setup_hpcc_input.sh
@@ -147,8 +147,8 @@ fi
 echo "End of entrypoint script. Interactive session will launch if specified."
 
 # if user sets interactive flag, starts up bash at end
-#if [[ "$interactive" == "true" ]]; then
-#	/bin/bash
-#fi
+if [[ "$interactive" == "true" ]]; then
+	/bin/bash
+fi
 
 
