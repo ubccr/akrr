ff8c78a3fedf874ac8ab0a2f6f8bb10b4bf57781
hoffmaps <hoffmaps@lemoyne.edu>
2019-06-07 13:29:39 -0400

    modified files to fit better with common practices

--- docker/hpcc/scripts/setup_run_hpcc.sh
+++ docker/hpcc/scripts/setup_run_hpcc.sh
@@ -5,24 +5,24 @@
 #echo "Number of arguments: $#"
 
 # initializing some variables
-num_args="$#"
-temp="hpcc/"
-hpcc_inputs_dir="$inputsLoc$temp"
-echo "hpcc_inputs_dir: $hpcc_inputs_dir"
+#num_args="$#"
+temp="hpcc"
+hpcc_inputs_dir="${INPUTS_LOC}/${temp}"
+echo "hpcc_inputs_dir: ${hpcc_inputs_dir}"
 
 # gets the number of cores of this machine
 cpu_cores="$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')"
-if [[ "$cpu_cores" == "1" ]]; then
+if [[ "${cpu_cores}" == "1" ]]; then
 	echo "Detected only one core. Counting processors instead"
 	cpu_cores="$(grep "processor" /proc/cpuinfo | wc -l)"
 fi
 
-echo "Number of cores detected: $cpu_cores"
+echo "Number of cores detected: ${cpu_cores}"
 
 # help text essentially
 usage()
 {
-    	echo "usage: setup_hpcc_inputs.sh [-h] [-i] [--norun] [-n NODES] [-ppn PROC_PER_NODE]"
+    	echo "usage: setup_hpcc_inputs.sh [-h] [-i] [--norun] [-n NODES] [-{ppn} PROC_PER_NODE]"
 	echo ""
     	echo " Options:"
     	echo "	-h | --help			Display help text"
@@ -30,7 +30,7 @@ usage()
 	echo " 	-i | --interactive		Start a bash session after the run"
 	echo "	--norun				Set if you don't want to immediately run hpcc"
 	echo "	-n NODES | --nodes NODES	Specify number of nodes hpcc will be running on"
-	echo "	-ppn PROC_PER_NODE | "
+	echo "	-{ppn} PROC_PER_NODE | "
 	echo "	--proc_per_node PROC_PER_NODE	Specify nymber of processes/cores per node" 
 	echo "					(if not specified, number of cpu cores is used,"
 	echo "					as found in /proc/cpuinfo)"
@@ -39,10 +39,10 @@ usage()
 # allows script to continue if the argument passed in is a valid number
 validate_number()
 {
-	echo "Testing entry: " $1
+	echo "Testing entry: ${1}"
 	# checking if the given argument is an integer
 	re='^[0-9]+$'
-	if ! [[ $1 =~ $re ]] ; then
+	if ! [[ ${1} =~ ${re} ]] ; then
    		echo "error: Entry is not an integer, as expected" >&2; exit 1
 	else
 		echo "Entry is valid"
@@ -52,9 +52,9 @@ validate_number()
 # setting default values for variables
 set_defaults()
 {
-	work_dir=$HOME # location where hpcc input file gets copied to
+	work_dir=${HOME} # location where hpcc input file gets copied to
 	nodes=1
-	ppn=$cpu_cores
+	ppn=${cpu_cores}
 	verbose=false
 	interactive=false
 	run_hpcc=true
@@ -63,7 +63,7 @@ set_defaults()
 set_defaults
 
 # loop through arguments - for each to one of them
-while [[ "$1" != "" ]]; do
+while [[ "${1}" != "" ]]; do
 	case $1 in
 		-h | --help)
 			usage
@@ -80,11 +80,11 @@ while [[ "$1" != "" ]]; do
 			;;
 		-n | --nodes)
 			shift
-			nodes=$1
+			nodes=${1}
 			;;
 		-ppn | --proc_per_node)
 			shift
-			ppn=$1
+			{ppn}=${1}
 			;;
 		*)
 			echo "Error: unrecognized argument"
@@ -96,49 +96,49 @@ while [[ "$1" != "" ]]; do
 done
 
 
-echo "nodes: $nodes"
-echo "ppn: $ppn"
-echo "interactive: $interactive"
+echo "nodes: ${nodes}"
+echo "{ppn}: ${ppn}"
+echo "interactive: ${interactive}"
 
 #validating that they are numbers
-validate_number $nodes
-validate_number $ppn
+validate_number ${nodes}
+validate_number ${ppn}
 
 # setting up paths to do the copying
 temp="x"
-hpcc_input_name="hpccinf.txt.$ppn$temp$nodes"
-input_file_path="$hpcc_inputs_dir$hpcc_input_name"
+hpcc_input_name="hpccinf.txt.${ppn}${temp}${nodes}"
+hpcc_input_full_path="${hpcc_inputs_dir}/${hpcc_input_name}"
 
-temp="/hpccinf.txt"
-dest_path=$work_dir$temp
+temp="hpccinf.txt"
+dest_path="${work_dir}/${temp}"
 
 # output for testing
-echo "Input file name: $hpcc_input_name"
-echo "Full path: $input_file_path"
-echo "Destination path: $dest_path"
+echo "Input file name: ${hpcc_input_name}"
+echo "Full path: ${hpcc_input_full_path}"
+echo "Destination path: ${dest_path}"
 
 # check if input file exists, if it does, copy it over
-if [[ -f "$input_file_path" ]]; then
-	cp $input_file_path $dest_path
-	echo "$hpcc_input_name copied over to $dest_path"
+if [[ -f "${hpcc_input_full_path}" ]]; then
+	cp ${hpcc_input_full_path} ${dest_path}
+	echo "${hpcc_input_name} copied over to ${dest_path}"
 else
-	echo "Error: $input_file_path does not exist"
+	echo "Error: ${hpcc_input_full_path} does not exist"
 	exit 1
 fi
 
 # go to working directory to run hpcc
-cd $work_dir
-echo "work dir: $work_dir"
+cd ${work_dir}
+echo "work dir: ${work_dir}"
 
 echo "Running appsigcheck..."
 # trying to run the script thing on hpcc
-$execsLoc/bin/appsigcheck.sh $hpccLoc
+${EXECS_LOC}/bin/appsigcheck.sh ${HPCC_LOC}
 
 # running hpcc with mpirun, where -np is number of cores for the machine
-if [[ "$run_hpcc" == "true" ]]; then
+if [[ "${run_hpcc}" == "true" ]]; then
 	echo "Running hpcc..."
-	$mpiLoc/mpirun -np $ppn $hpccLoc
-	echo "Complete! hpccoutf.txt is in $work_dir"
+	${MPI_LOC}/mpirun -np ${ppn} ${HPCC_LOC}
+	echo "Complete! hpccoutf.txt is in ${work_dir}"
 	echo "cat output to standard out:"
 	cat hpccoutf.txt
 fi
@@ -147,7 +147,7 @@ fi
 echo "End of entrypoint script. Interactive session will launch if specified."
 
 # if user sets interactive flag, starts up bash at end
-if [[ "$interactive" == "true" ]]; then
+if [[ "${interactive}" == "true" ]]; then
 	/bin/bash
 fi
 
