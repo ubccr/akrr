c09d7bfc7f64320d30b257719f7d5ee44eef1029
Nikolay Simakov <nikolays@buffalo.edu>
2019-08-23 18:02:48 -0400

    updated docker and shippable files to run test as akrruser

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Run regtest1 in docker
-FROM nsimakov/akrr_ready_centos_slurm_single_host_wlm:last
+FROM nsimakov/akrr_ready_centos_slurm_single_host_wlm:latest
 
 LABEL description="image to run tests manually"
 
@@ -7,7 +7,6 @@ LABEL description="image to run tests manually"
 VOLUME /home/akrruser/src/github.com/ubccr/akrr
 
 ENV REPO_FULL_NAME=ubccr/akrr
-COPY ./docker/utils/cmd_setup ./docker/utils/cmd_start ./docker/utils/cmd_stop /usr/local/sbin/
 
 ENTRYPOINT ["/usr/local/sbin/cmd_start"]
 CMD ["-set-no-exit-on-fail", "self_contained_slurm_wlm", "bash_akrruser"]
--- tests/regtest1/run_test_on_shippable.sh
+++ tests/regtest1/run_test_on_shippable.sh
@@ -1,27 +1,54 @@
 #!/usr/bin/env bash
+echo PATH=$PATH
+echo USER=$USER
+# This script is executed in docker as local user
+# either on shippable or in local docker run
+# mimicing shippable directory layout
+#
+# Local user do sudo to start-up nessesary services
+# most test done as local user
+
 export REPO_FULL_NAME=${REPO_FULL_NAME:-ubccr/akrr}
-export SETUP_WAY=${1:-dev}
+export AKRR_SETUP_WAY=${AKRR_SETUP_WAY:-dev}
+export AKRR_SETUP_WAY=${1:-$AKRR_SETUP_WAY}
+
+# line source code to user's home, on local docker run
+# /root/src/github.com/$REPO_FULL_NAME can be mounted from host
+sudo chmod 755 /root
+sudo chown -R "$USER":"$USER" "/root/src"
+ln -s "/root/src/github.com/$REPO_FULL_NAME" "/home/$USER/akrr_src"
+
+# exit on first error
 set -e
+
+# before exit on first error print line number where error occured
+trap 'catch $? $LINENO' ERR
+catch() {
+  echo "Error $1 occurred on line $2"
+}
 
 highlight='-e "\e[32,1m[AKRR_Reg_Test:0]\e[0m"'
 
 # Start all daemons
-cmd_start self_contained_slurm_wlm
+sudo /usr/local/sbin/cmd_start self_contained_slurm_wlm
+
+# check slurm and log processes for debugging
 squeue
 sinfo
 ps -Af
 
-cd /root/src/github.com/${REPO_FULL_NAME}
+cd "/home/$USER/akrr_src"
 
 #install source code
-if [ "${SETUP_WAY}" == "rpm" ]; then
+if [ "${AKRR_SETUP_WAY}" == "rpm" ]; then
     # Build RPM
     ./setup.py bdist_rpm
-
     # Install RPM
-    rpm -Uvh dist/akrr-*.noarch.rpm
-elif [ "${SETUP_WAY}" == "dev" ]; then
-    ./setup.py develop
+    sudo rpm -Uvh dist/akrr-*.noarch.rpm
+elif [ "${AKRR_SETUP_WAY}" == "dev" ]; then
+    sudo ./setup.py develop
+elif [ "${AKRR_SETUP_WAY}" == "src" ]; then
+    echo "Running in source"
 else
     echo "Unknown setup.py call"
     exit 1
@@ -48,7 +75,8 @@ export PATH=/root/src/github.com/${REPO_FULL_NAME}/tests/bin:$PATH
 #remove akrr from crontab to avoid uncontrolled AKRR launch
 echo $highlight "remove akrr from crontab to avoid uncontrolled AKRR launch"
 cd tests/regtest1
-/root/src/github.com/${REPO_FULL_NAME}/tests/bin/akrrregtest remove --crontab --crontab-remove-mailto
+# @todo add test with crontab
+"/home/$USER/akrr_src/tests/bin/akrrregtest" remove --crontab --crontab-remove-mailto
 cd ../..
 
 # Rerun unit tests and run system tests together
